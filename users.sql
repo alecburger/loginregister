ab2ea0664e8f0d5bb44ed630a06d2a69daaaeb62
alecburger <alecburgerspam@gmail.com>
2014-01-11 22:39:32 +0100

    Update users.sql

--- users.sql
+++ users.sql
@@ -9,13 +9,3 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
-INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
-(1, 'alecburger', 'burger', 'alec@kpnmail.nl'),
-(2, 'floris', 'burger', 'floris@f-j-a.nl'),
-(3, 'teamviewer', '1234', 'teamviewer@gmail.com'),
-(4, 'jan', 'janssen', 'janjanssen@gmail.com'),
-(5, 'test', 'test', 'test@test.nl'),
-(6, 'blabla', 'blabla', 'bla@bla.com'),
-(7, 'jamilzolnet', 'wachtwoord', 'jzolnet@gmail.com'),
-(8, 'testg', 'testg', 'testg.@'),
-(9, 'net', 'net', 'net@.');
